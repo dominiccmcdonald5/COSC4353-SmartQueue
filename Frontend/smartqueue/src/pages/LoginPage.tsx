4953c8f1d5eb5e97c0cc8434193839164c72e187
dominiccmcdonald5 <dominiccmcdonald@gmail.com>
2026-02-14 13:05:24 -0600

    Segmenting the CSS files to be in different files

--- Frontend/smartqueue/src/pages/LoginPage.tsx
+++ Frontend/smartqueue/src/pages/LoginPage.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import '../styling/LoginSignup.css';
 
 const LoginPage: React.FC = () => {
   const [email, setEmail] = useState('');
